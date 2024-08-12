d084379e5e56f563f4bbd68b0c0e400d10065143
xxieen <xieenenenen@outlook.com>
2024-08-12 15:32:07 +0800

    socketIO

--- src/TcpServer/Socket.cc
+++ src/TcpServer/Socket.cc
@@ -1,30 +1,19 @@
 #include "../../include/TcpServer/Socket.h"
 
-Socket::Socket()
-:_sockfd(0)
-{
-    _sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(_sockfd < 0)
-    {
-        perror("scoket");
-    }
+Socket::Socket() : _sockfd(0) {
+  _sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (_sockfd < 0) {
+    perror("scoket");
+  }
 }
 
-Socket::~Socket()
-{
-    close(_sockfd);
-}
+Socket::~Socket() { close(_sockfd); }
 
-Socket::Socket(int fd)
-: _sockfd(fd)
-{}
+Socket::Socket(int fd) : _sockfd(fd) {}
 
-int Socket::fd() const 
-{
-    return _sockfd;
-}
+int Socket::fd() const { return _sockfd; }
 
-void Socket::shutdownWrite()
+void Socket::shutdownWrite() // 关闭写通道
 {
-	shutdown(_sockfd, SHUT_WR);
+  shutdown(_sockfd, SHUT_WR);
 }
